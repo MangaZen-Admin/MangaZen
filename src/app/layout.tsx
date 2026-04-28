8490a661db3352163d0627b79eb4540c2339edeb
MangaZen Admin <admin.mangazen@gmail.com>
2026-04-28 12:34:44 -0300

    feat: speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import "./globals.css";
@@ -81,6 +82,7 @@ export default async function RootLayout({
             <SiteFooter />
           </NextIntlClientProvider>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
